a935f5da0d8b7ff390f670aea352e8e150d23ce6
erkncvk <erkancevik@gmail.com>
2023-11-28 16:17:20 +0300

    Projeyi gite ekle

---
+++ app/src/main/java/com/example/javainstagramclone/UploadActivity.java
@@ -0,0 +1,22 @@
+package com.example.javainstagramclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class UploadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload);
+    }
+
+    public void uploadButtonClicked(View view){
+
+    }
+
+    public void selectImageClicked(View view){
+
+    }
+}
